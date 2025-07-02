bd0c70fe5125343abdb7566a17a5003476ae51ac
Ayush Singh <ayush.singh230306@gmail.com>
2025-07-03 04:26:06 +0530

    Completed full BLOG Website

---
+++ PROJECTS/Blog Website/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const path = require('path');
+const hbs = require('hbs');
+const PORT = 4444;
+
+app.use(express.urlencoded({extended:true}));
+app.set('view engine','hbs')
+app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname,'views','partials'));
+
+const routeHandler = require('./routes/blogsRoute');
+app.use('/',routeHandler);
+
+mongoose.connect('mongodb://127.0.0.1:27017/myBlogs')
+    .then(()=>{
+        app.listen(PORT,()=>{
+            console.log(`http://localhost:${PORT}`);
+        });
+    })
+    .catch(err=>{
+        console.log("mongoose couldnt connect");
+    })
